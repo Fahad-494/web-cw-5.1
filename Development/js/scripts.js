b96d9485bab86ba0cf767b359513cd78b0c696d6
Fahad-494 <66741156+e2id@users.noreply.github.com>
2020-06-27 20:37:23 +0300

    خلصت

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,41 @@
 // السؤال الأول
 // 1
-
+const name = "fahad fuhid alsubaei"
 // 2
-
+let counter = 17
 // 3 and 4
-
+console.log(name+" "+" and I am "+counter)
 // 5 and 6
-
+counter *= 5
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbise=["swimming", "shooting"," scuba-diving"]
 // 2
-
+hobbise.push("progeramming")
+hobbise[4]= "progeramming"
+console.log(hobbise)
 // 3
+hobbise.pop()
+console.log(hobbise)
 
 // 4
-
+let properties = {
+    name:"Web" ,
+    language:"Html" ,
+}
 // 5
-
+properties= teacher=["Hussain","Nancy","Menawer","Moudhi","Rahaf"]
 // 6
-
+console.log("Hussain Almousawi")
 // 7
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
-
+properties[3="I like orange"]
 // 2
-
+console.log(properties[3])
 // console results => console.png
